d51c3e8bc99afc46730685c14b48f4b2fe1f953e
xueke477 <xueke.kent@gmail.com>
2022-09-06 14:46:27 -0400

    test

--- tests/test_mod1.py
+++ tests/test_mod1.py
@@ -5,5 +5,9 @@ def test_add() -> None:
     assert add(1, 2) == 3
 
 
-def test_add_unhappy() -> None:
+def test_add_happy() -> None:
     assert add(1, 3) == 4
+
+
+def test_add_happy2() -> None:
+    assert add(1, 4) == 5
